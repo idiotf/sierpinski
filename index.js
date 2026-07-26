cb862edba9f69249090c9b8923402eeb25dc0ff3
idiotf <aqu3180@naver.com>
2026-07-26 19:29:59 +0900

    Auto repeat drawing animations

--- index.js
+++ index.js
@@ -12,41 +12,69 @@ async function lineTo(x, y) {
   await new Promise(resolve => setTimeout(resolve, delay))
 }
 
-async function recursive(
-  step,
-  x1, y1,
-  x2, y2,
-  x3, y3
-) {
-  if (step == 0) return
+let recursive
+const methods = [
+  async function Pyramid(step, x1, y1, x2, y2, x3, y3) {
+    if (step == 0) return
 
-  const x12 = (x1 + x2) * 0.5, y12 = (y1 + y2) * 0.5
-  const x23 = (x2 + x3) * 0.5, y23 = (y2 + y3) * 0.5
-  const x13 = (x1 + x3) * 0.5, y13 = (y1 + y3) * 0.5
+    const x12 = (x1 + x2) * 0.5, y12 = (y1 + y2) * 0.5
+    const x23 = (x2 + x3) * 0.5, y23 = (y2 + y3) * 0.5
+    const x13 = (x1 + x3) * 0.5, y13 = (y1 + y3) * 0.5
 
-  await recursive(
-    step - 1,
-    x1, y1,
-    x12, y12,
-    x13, y13
-  )
-  await lineTo(x13, y13)
-  await lineTo(x12, y12)
-  await recursive(
-    step - 1,
-    x12, y12,
-    x2, y2,
-    x23, y23
-  )
-  await lineTo(x23, y23)
-  await lineTo(x13, y13)
-  await recursive(
-    step - 1,
-    x13, y13,
-    x23, y23,
-    x3, y3
-  )
-}
+    await recursive(
+      step - 1,
+      x1, y1,
+      x12, y12,
+      x13, y13
+    )
+    await lineTo(x13, y13)
+    await lineTo(x12, y12)
+    await recursive(
+      step - 1,
+      x12, y12,
+      x2, y2,
+      x23, y23
+    )
+    await lineTo(x23, y23)
+    await lineTo(x13, y13)
+    await recursive(
+      step - 1,
+      x13, y13,
+      x23, y23,
+      x3, y3
+    )
+  },
+  async function Leaf(step, x1, y1, x2, y2, x3, y3) {
+    if (step == 0) return
+
+    const x12 = (x1 + x2) * 0.5, y12 = (y1 + y2) * 0.5
+    const x23 = (x2 + x3) * 0.5, y23 = (y2 + y3) * 0.5
+    const x13 = (x1 + x3) * 0.5, y13 = (y1 + y3) * 0.5
+
+    await lineTo(x13, y13)
+    await recursive(
+      step - 1,
+      x13, y13,
+      x1, y1,
+      x12, y12
+    )
+    await lineTo(x12, y12)
+    await recursive(
+      step - 1,
+      x12, y12,
+      x2, y2,
+      x23, y23
+    )
+    await lineTo(x23, y23)
+    await recursive(
+      step - 1,
+      x23, y23,
+      x3, y3,
+      x13, y13
+    )
+    await lineTo(x13, y13)
+  },
+]
 
 async function sierpinski(
   step,
@@ -73,9 +101,14 @@ ctx.lineJoin = 'round'
 
 const sqrt3_2 = Math.sqrt(3) * 0.5
 
-sierpinski(
-  steps,
-  400 - sqrt3_2 * 320, 600,
-  400, 120,
-  400 + sqrt3_2 * 320, 600
-)
+for (;;) {
+  for (recursive of methods) {
+    await sierpinski(
+      steps,
+      400 - sqrt3_2 * 320, 600,
+      400, 120,
+      400 + sqrt3_2 * 320, 600
+    )
+    await new Promise((resolve) => setTimeout(resolve, 1500))
+  }
+}
